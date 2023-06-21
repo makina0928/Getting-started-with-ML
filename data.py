02c7cfef6e696cc6b2855f5b1cc1cdb896b92a02
Mulimbika Makina <makinamulimbika0928@gmail.com>
2023-06-20 21:48:51 -1200

    add streamlit file

---
+++ data.py
@@ -0,0 +1,9 @@
+import stramlit as st
+import pandas as pd
+import plotly.express as px
+from streamlit_option_menu import option_menu
+from numerize.numerize import numerize
+
+st.set_page_conf(page_title = "Dashboard", layout = "wide")
+st.subheader("Data analyst")
+st.markdown("##")
